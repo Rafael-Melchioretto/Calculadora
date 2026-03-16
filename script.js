69e85f93c2c7f7bbc44903b3e4722f78641f5a42
Rafael-Melchioretto <rafael_melchioretto@estudante.sesisneai.org.br>
2026-03-16 09:35:26 -0300

    feat - adiconado todo código no javascript com dados para o calculo e caso haja erro no visor.

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function adicionar (valor){
 function limpar () {
     document.getElementById('visor').value = '';
 }
+
+function apagar() {
+    var visor = document.getElementById('visor');
+    visor.value = visor.value.slice(0, -1);
+}
+
+function calcular() {
+    var visor = document.getElementById('visor');
+    try{
+        // Usa a função eval() para calcular o resultado
+        // Nota: eval() pode ser perigoso com entrada não confiável
+        visor.value = eval(visor.value);
+    } catch(error) {
+        //Se houver um erro na expressão vai mostra Erro no visor
+        visor.value = 'Erro'
+    }
+}
